10db9a3e021c59d506f9d3c7f6ba874313992eec
AnuragMunda <anurag.munda005@gmail.com>
2026-03-14 22:01:53 +0530

    code changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
 "use client";
 
+import { DatePickerWithRange } from "@/components/datePickerWithRange";
+import { MultiLineChart } from "@/components/multiLineChart";
+import { Button } from "@/components/ui/button";
+import { Slider } from "@/components/ui/slider";
 import { fetchActualData, fetchForecastedData } from "@/lib/fetch";
 import { buildSeries } from "@/lib/helper";
 import {
@@ -7,12 +11,13 @@ import {
   WindGenerationActual,
   WindGenerationForecast,
 } from "@/lib/types";
-import Image from "next/image";
+import { useTheme } from "next-themes";
 import { useEffect, useState } from "react";
 
 export default function Home() {
   /*------------------------ USESTATES ------------------------*/
 
+  const { theme, setTheme } = useTheme();
   const [actuals, setActuals] = useState<WindGenerationActual[]>([]);
   const [forecasted, setForecasted] = useState<
     Map<string, WindGenerationForecast[]>
@@ -54,63 +59,27 @@ export default function Home() {
   /*------------------------ TSX ------------------------*/
 
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <div className="flex min-h-screen">
+      <main className="w-full flex flex-col gap-8 items-center">
+        <header className="w-full border-b-2 py-6 px-10 flex justify-between items-center">
+          <h2 className="text-xl tracking-wide font-semibold">Forecast Monitoring App</h2>
+          <Button
+            onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+            {theme === "dark" ? "☀️" : "🌙"}
+          </Button>
+        </header>
+
+        <div className="w-full flex">
+          <DatePickerWithRange />
+          <Slider
+            defaultValue={[75]}
+            max={100}
+            step={1}
+            className="mx-auto w-full max-w-xs"
+          />
         </div>
+        <MultiLineChart />
       </main>
     </div>
   );
